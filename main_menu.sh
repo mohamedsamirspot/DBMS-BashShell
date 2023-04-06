f4e6a57a921e613e75694fffbe6444ec8f385147
Mohamed Samir <mohamedsamirspot@gmail.com>
2023-04-07 01:43:07 +0200

    Validations

--- main_menu.sh
+++ main_menu.sh
@@ -2,43 +2,27 @@
 
 function create_database() {
     read -p "Enter Database Name: " dbname
-    if [[ $dbname =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
-        #the "=" operator test for string equality, "=~" operator used to match a string with regular expression
-        # "^" indicates the start of the string
-        # "[a-zA-Z_]" matches any uppercase or lowercase letter or underscore
-        # "[a-zA-Z0-9_]*" the rest matches zero or more occurrences of any uppercase or lowercase letter or digit or underscore
-        # $ symbol at the end of the regex which indicates that the string should end with the preceding pattern onlyyy and to handle spaces
+    # check database name
+    if [[ "$dbname" =~ ^[a-zA-Z_][a-zA-Z0-9_]{0,127}$ ]]; then
         dbname=${dbname,,}
-        # braces to combine the variable dbname and the ,, which is a Bash-specific feature to convert all letters to lower case
-        # this dbname=${dbname},, or this dbname=$dbname,, will name like that databasename,,
-        if [ -d $dbname ]; then
+        if [ -d "$dbname" ]; then
             echo "Database already exists!"
         else
-            mkdir $dbname
+            mkdir "$dbname"
             echo "Database created successfully!"
         fi
     else
-        echo "Invalid database name! The name should only contain letters, numbers, underscores and without any spaces, and should start with an underscore or a letter (not a number)."
+        echo -e "Invalid database name! \n\"The name should only contain letters, numbers, underscores and without any spaces, and should start with an underscore or a letter (not a number) with minimum 1 and maximum 128 characters.\""
     fi
     read -n1 -p "Press any key to continue..."
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------
 function list_databases() {
     echo "List of Databases:"
-    #check if there is dires found
-    if [ $(find . -maxdepth 1 -type d -not -name '.' | sed '1d' | wc -l) -gt 0 ]; then
-        # . after find just the path paramter it can be only find
-        # "$(...)" is used to capture the output of the previous commands and store it as a string to compare with -gt 0
-        # "find" command search files or dires on cur dir
-        # -maxdepth 1 specifies max depth of the search and 1 means that the search will be only in the cur dir and not subdirs
-        # -type d specifies that search should only return dires
-        # -not -name '.' specifies the search exclude the cur dir .
-        # sed '1d' removes the first line of the find output which is the name of ./.git dir or sed '1,2d' to removes first two lines
-        # wc -l counts number of lines in the output which corresponds to the number of dirs without . or ./.git and -l to make wc count the line not the words
+    # check if there is directories in the current directory and exclude any errors
+    if [ "$(ls -d */ 2>/dev/null)" ]; then
         for d in */; do
-            echo "${d%/}"
-            # The "*/" all dires in the cur dir
-            # The "${d%/}" expression removes "/" character from the dir
+            echo ${d%/} # remove / from directory name
         done
     else
         echo "No databases found!"
@@ -48,23 +32,21 @@ function list_databases() {
 #-------------------------------------------------------------------------------------------------------------------------------------------------
 function connect_to_database() {
     read -p "Enter Database Name: " dbname
-    if [ -d $dbname ]; then
-        cd $dbname
-        ../submenu.sh $dbname
+    # check if there is a directory (database) with the input name and cd to it
+    if [ -d "$dbname" ]; then
+        cd "$dbname"
+        ../submenu.sh "$dbname"
     else
         echo "Database does not exist!"
-        read -n1 -p "Press any key to continue..."
     fi
+    read -n1 -p "Press any key to continue..."
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------
 function drop_database() {
     read -p "Enter Database Name: " dbname
-    if [ -d $dbname ]; then
-        #[ or [[ the two will work but [ will be compatatble with more shells
-        # -d option to check if the dir is in the cur dir or not
-        rm -r $dbname
-        # -r to remove recursivly
-        # rmdir remove only emptyyy dirs
+    # check if there is a directory (database) with the input name
+    if [ -d "$dbname" ]; then
+        rm -r "$dbname"
         echo "Database dropped successfully!"
     else
         echo "Database does not exist!"
@@ -73,7 +55,6 @@ function drop_database() {
 }
 #------------------------------------------------------------------------------------------------------------------------------------------------
 while true; do
-    # ; to write muliple commands or keywords in one line
     clear
     echo "Main Menu:"
     echo "1. Create Database"
@@ -81,17 +62,13 @@ while true; do
     echo "3. Connect To Database"
     echo "4. Drop Database"
     read -p "Enter your choice [1-4]: " choice
-
-    case $choice in
+    case "$choice" in
     1) create_database ;;
-        # could be e) or any characters but not 1] or 1}
     2) list_databases ;;
     3) connect_to_database ;;
     4) drop_database ;;
     *)
-        echo "Invalid option. Press any key to continue..."
-        read -n1
+        read -p "Invalid option. Press any key to continue..." -n1
         ;;
-        # -n1 option to read one char and run the script again
     esac
 done
--- submenu.sh
+++ submenu.sh
@@ -2,83 +2,58 @@
 
 function create_table {
     read -p "Enter table name: " table_name
-    # Validate table name format
-    if [[ $table_name =~ ^[_a-zA-Z][_a-zA-Z0-9]*$ ]]; then
+        # Validate table name format
+    if [[ "$table_name" =~ ^[_a-zA-Z][_a-zA-Z0-9]{0,127}$ ]]; then
         # Convert table name to lowercase
         table_name=${table_name,,}
-
         table_file=$table_name.txt
-
+        # check if table exist
         if [ -f $table_file ]; then
             echo "Table already exists!"
             return
         fi
     else
-        echo "Invalid table name! The name should only contain letters, numbers, underscores and without any spaces, and should start with an underscore or a letter (not a number)."
+        echo "Invalid table name! The name should only contain letters, numbers, underscores and without any spaces, and should start with an underscore or a letter (not a number) with minimum 1 and maximum 128 characters.."
         return
     fi
 
     # Ask for column names and data types
     echo -e "Enter column names and data types in this format (e.g. id:int(pk) name:string age:int) \nNote: Ensure that the pk is must and be the first column"
-    # The -e option in the echo command enables the interpretation of escape sequences like \n
     read -p "Columns: " columns
 
     # Validate columns
     # Each column is a pair of name:type separated by space, appended with (pk) for primary key (first column)
-    # Allowed data types: int, string
-    # Example: id:int(pk) name:string age:int
+    # Allowed data types: int, string --> Example: id:int(pk) name:string age:int
     regex='^([a-zA-Z_]\w*):(int|string)(\(pk\)) *( [a-zA-Z_]\w*:(int|string))*$'
-    if ! [[ $columns =~ $regex ]]; then
+    if ! [[ "$columns" =~ $regex ]]; then
         echo "Invalid columns format!"
         return
     fi
-
-    # ^ asserts the start of the string
-    # ([a-zA-Z_]\w*) matches a word that starts with a letter or underscore, followed by zero or more letters, digits, or underscores
-    # \w to accept digits
-    # : matches a colon
-    # (int|string) matches either "int" or "string"
-    # (\(pk\)) matches "(pk)" in parentheses
-    # * matches zero or more occurrences of the preceding space character
-    # ( [a-zA-Z_]\w*:(int|string))* matches zero or more occurrences of a space character, followed by a word that starts with a letter or underscore, a colon, and either "int" or "string"
-    # $ asserts the end of the string
-
-    columns=${columns,,} # convert upper to lower cases
-    # Write column names and data types to table file
-    echo $columns | sed -E 's/(:int|\:string)(\(pk\))? /&\t\|/g' >$table_file
-
-    # -E option in the sed command can make it easier to write and read regular expressions, especially when dealing with more complex patterns.
-    #     s/ : This indicates a substitution operation.
-    # (:int|:string) : This is a group that matches either the string ":int" or the string ":string". The vertical bar (|) is used to indicate alternation between the two possible matches. In regular expressions, parentheses are used to group elements together.
-    # (pk)? : This is a group that matches the string "(pk)" optionally. The question mark indicates that the previous group is optional.
-    # space after (pk)? is important for formatting
-    # /&\t|/g : means to replace the matched pattern with itself followed by a tab character ("\t") and a vertical bar ("|") This is the replacement string, which replaces any matches found by the pattern. It consists of three parts:
-    # & : This refers to the entire matched substring.
-    # \t : This is a tab character.
-    # | : This is a vertical bar character, which is often used to separate columns in data.
-    # The "g" at the end of the pattern indicates that the substitution should be applied globally, replacing all occurrences in the input string.
+    # convert upper to lower cases
+    columns=${columns,,}
+    # Write column names and data types to table file with the appropriate formatting
+    echo $columns | sed -E 's/(:int|\:string)(\(pk\)) /&\t\|/g' >$table_file
     echo "Table $table_name created successfully."
 }
 #----------------------------------------------------------------------------------------------------------------------------------------------
 function list_tables {
-    if [ ! $(find -name '*.txt') ]; then
-        # ! inside [] or outside nothing differnt
+    if [ ! $(ls *.txt 2>/dev/null) ]; then
         echo "No tables exist in this database."
     else
         echo "Tables in the database:"
         for file in *.txt; do
-            # Extract the table name from the file name
             table_name=${file%.txt}
-            echo - $table_name
+            echo - "$table_name"
         done
     fi
 }
 #----------------------------------------------------------------------------------------------------------------------------------------------
 function drop_table {
     read -p "Enter the name of the table to drop: " table_name
-    table_file=${table_name}.txt
-    if [ -f $table_file ]; then
-        rm $table_file
+    table_file=$table_name.txt
+    # check if table exist in current directory
+    if [ -f "$table_file" ]; then
+        rm "$table_file"
         echo "Table dropped successfully!"
     else
         echo "Table does not exist!"
@@ -89,7 +64,7 @@ function insert_into_table {
     read -p "Enter table name: " table_name
     table_file=$table_name.txt
 
-    # Check if table file exists
+    # Check if table file exists in current directory
     if ! [ -f "$table_file" ]; then
         echo "Table does not exist!"
         return
@@ -102,18 +77,20 @@ function insert_into_table {
     # The output of tr is then assigned to the variable column_names.
 
 
-    # Prompt user to enter data for each column
+    # Prompt user to enter data for each column (each line of varable column_names)
     data=""
     for column in $column_names; do
         read -p "Enter value for column '$column': " value
 
         # Check if the column type is 'int' and validate the input accordingly
         if [[ "$column" == *"int"* ]]; then
-            if ! [[ "$value" =~ ^[0-9]+$ ]]; then
-                echo "Invalid column data: $value is not an integer for column $column!"
-                return
-            fi
+             if ! [[ $value =~ ^-?[0-9]+$ ]] || (( value < -2147483648 )) || (( value > 2147483647 )); then
+             # -? optional negative sign
+                   echo "Invalid column data: $value is not an integer within the range of a 32-bit signed integer for column $column!"
+                   return
+             fi
         fi
+
 
         # check for | (no pipes allowed)
         if [[ "$value" == *"|"* ]]; then
@@ -123,42 +100,41 @@ function insert_into_table {
 
         # Check if the column is the primary key column and if the entered value is already in the table
         if [[ "$column" == *"(pk)"* ]]; then
-            if grep -q "^$value" "$table_file"; then
+            if grep -q "^$value\b" "$table_file"; then
             # grep searches for a pattern (^$value) in the file $table_file means to match the beginning of a line (^) followed by the value of $value
             # -q option suppresses the output of grep and only returns the exit status. If grep finds a match, the exit status is 0 else exit status is 1, which means failure.
+            # The regular expression ^$value\b matches lines that start with the value in the $value variable, followed by a word boundary (\b). This ensures that the match is for the first word of the line, rather than any instance of the value appearing within the line.
                 echo "Invalid column value: $value already exists in $column column!"
                 return
             fi
         fi
-
         data+="|$value"$'\t\t'
+        # The $'...' syntax is used to enable escape sequences in strings.
     done
+
     # Remove first pipe character
     data="${data:1}"
-    # By using ${data:1} 1 is the index, the first character (the delimiter) is removed from the beginning of the string, and the resulting substring is stored back in the data variable. The modified data string can then be used in subsequent commands without the delimiter at the beginning.
-
     # Write data to table file
-    echo $data >> $table_file
-    # > overwrite       >> append
+    echo $data >> "$table_file"
     echo "Data inserted into $table_name successfully."
 }
 #----------------------------------------------------------------------------------------------------------------------------------------------
 function select_from_table {
     read -p "Enter the name of the table: " table_name
     table_file=$table_name.txt
-    if [ ! -f $table_file ]; then
+    # check if table exist in current directory
+    if [ ! -f "$table_file" ]; then
         echo "Table does not exist!"
         return
     fi
 
     read -p "Select all table or select by primary key? (all/key): " option
-    if [ $option == "all" ]; then
-        echo "$(cat $table_file)"
-        # "" to prevent cat from printing in one line
-    elif [ $option == "key" ]; then
+    if [ "$option" == "all" ]; then
+        cat "$table_file"
+    elif [ "$option" == "key" ]; then
         read -p "Enter the primary key value: " pk_value
-        pk_output=$(grep ^$pk_value $table_file)
-        if [[ ! $pk_output ]]; then
+        pk_output=$(grep "^$pk_value\b" "$table_file")
+        if [[ ! "$pk_output" ]]; then
             echo "not found"
         else
             echo "$pk_output"
@@ -180,22 +156,22 @@ function delete_from_table {
     fi
 
     read -p "Do you want to delete all records from '$table_name' (Y/N)? " delete_all
-    if [[ $delete_all == "Y" || $delete_all == "y" ]]; then
+    if [[ "$delete_all" == "Y" || "$delete_all" == "y" ]]; then
         # Delete all lines except the first line
         sed -i '2,$d' "$table_file"
         echo "All records deleted from table '$table_name' successfully."
-    elif [[ $delete_all == n || N ]]; then
+    elif [[ "$delete_all" == n || N ]]; then
         read -p "Enter value the pk for the record to delete: " pk_value
-        if [[ ! $pk_value ]]; then
+        if [[ ! "$pk_value" ]]; then
             echo "Primary key value is empty. Please enter a value."
             return
         fi
         if grep -q "^$pk_value" "$table_file"; then
             # grep searches for a pattern (^$pk_value) in the file $table_file means to match the beginning of a line (^) followed by the value of $pk_value
             # -q option suppresses the output of grep and only returns the exit status. If grep finds a match, the exit status is 0 else exit status is 1, which means failure.
-            sed -i "/^$pk_value/d" "$table_file"
+            sed -i "/\<$pk_value\>/d" "$table_file"
             # option -i means to edit the file in place it modifies the contents of the file directly instead of printing the modified text to the standard output
-            # /^$pk_value/d means to delete any line that begins with the value of $pk_value
+            # /\<$pk_value\>/d means to delete any line that begins with a full word with the value of $pk_value
             echo "Record with primary key '$pk_value' deleted from table '$table_name' successfully."
         else
             echo "Record with primary key '$pk_value' does not exist in table '$table_name'!"
@@ -216,17 +192,11 @@ function update_table {
         return
     fi
 
-    # Read column names and primary key column name from table file
-    column_names=$(head -n 1 "$table_file" | tr '|' '\n')
-    # It reads the first line of a file specified by the variable $table_file.
-    # It pipes this line to the tr command which translates or replaces all occurrences of the | character with a newline character \n.
-    # The output of tr is then assigned to the variable column_names.
-
     # Prompt user to enter primary key value
     read -p "Enter the primary key value of the row to update: " pk_value
 
     # Find the row with the specified primary key value
-    row=$(grep "^$pk_value\s\+" "$table_file")
+    row=$(grep "^$pk_value" "$table_file")
     # to find the pk with this value pk_value followed by a space
     # \s is not like s/ in the create function it is a regular expression metacharacter that matches any whitespace character, including space, tab, newline, and other whitespace characters.
 
@@ -235,22 +205,29 @@ function update_table {
         return
     fi
 
+    # Read column names and primary key column name from table file
+    column_names=$(head -n 1 "$table_file" | tr '|' '\n')
+    # It reads the first line of a file specified by the variable $table_file.
+    # It pipes this line to the tr command which translates or replaces all occurrences of the | character with a newline character \n.
+    # The output of tr is then assigned to the variable column_names.
+
+
     # Prompt user to enter new values for each column
     new_data=""
     for column in $column_names; do
         # Check if the column is the primary key column
         if [[ "$column" == *"(pk)"* ]]; then
             # Add the primary key value to the new data
-            new_data+="$pk_value"$'\t\t'
+            new_data+="$pk_value"
         else
             # Prompt user to enter a new value for the column
             read -p "Enter new value for column $column: " new_value
-
             # Check if the column type is 'int' or 'string' and validate the input accordingly
             if [[ "$column" == *"int"* ]]; then
-                if ! [[ "$new_value" =~ ^[0-9]+$ ]]; then
-                    echo "Invalid column data: $new_value is not an integer for column $column!"
-                    return
+                if ! [[ $value =~ ^-?[0-9]+$ ]] || (( value < -2147483648 )) || (( value > 2147483647 )); then
+                     # -? optional negative sign
+                   echo "Invalid column data: $value is not an integer within the range of a 32-bit signed integer for column $column!"
+                   return
                 fi
             elif [[ "$column" == *"string"* ]]; then
                 if [[ "$new_value" == *"|"* ]]; then
@@ -264,22 +241,12 @@ function update_table {
 
     # Replace the old row with the new data
     sed -i "s/^$pk_value\s\+.*/$new_data/" "$table_file"
-
-    # sed -i: This runs sed in "in-place" mode, meaning that it will modify the file directly rather than printing the modified contents to standard output.
-    # "s/^$pk_value\s\+.*/$new_data/" is the substitution command that specifies what text to replace and with what. Here's what each part of the command does:
-    # s/ indicates that this is a substitution command.
-    # ^$pk_value is a regular expression that matches the start of a line (^) followed by the value of $pk_value variable, which is a pattern or value to search for.
-    # \s\+ matches one or more whitespace characters.
-    # .* matches any number of characters (except for a newline).
-    # $new_data is the replacement text, which is a variable containing the new value that will replace the old value.
-    # " and / are delimiter characters that surround the command and separate its parts.
-    # "$table_file" is the path to the file that will be edited. The $table_file variable contains the name of the file to be modified.
+    # find a line starts with pk_value followd by one or more white spaces and any number of anything after that using (.*)
     echo "Row with primary key value '$pk_value' updated successfully."
 }
 #----------------------------------------------------------------------------------------------------------------------------------------------
 clear
-echo "Connected to database: $1"
-echo "=============================="
+echo -e "Connected to database: $1 \n-------------------------------------------"
 PS3="Enter your choice [1-7] or 8 to disconnect: "
 select opt in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table" "Disconnect Database"; do
     case $opt in
@@ -298,38 +265,5 @@ select opt in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Sel
     esac
     read -p "Press any key to continue..." -n1
     clear
-    echo "Connected to database: $1"
-    echo "=============================="
+    echo -e "Connected to database: $1 \n-------------------------------------------"
 done
-
-
-
-# while true; do
-#     clear
-#     echo "Connected to database: $1"
-#     echo "=============================="
-#     echo "1. Create Table"
-#     echo "2. List Tables"
-#     echo "3. Drop Table"
-#     echo "4. Insert into Table"
-#     echo "5. Select From Table"
-#     echo "6. Delete From Table"
-#     echo "7. Update Table"
-#     echo "0. Disconnect Database"
-#     read -p "Enter your choice [1-7] or 0 to disconnect: " choice
-#     case $choice in
-#     1) create_table ;;
-#     2) list_tables ;;
-#     3) drop_table ;;
-#     4) insert_into_table ;;
-#     5) select_from_table ;;
-#     6) delete_from_table ;;
-#     7) update_table ;;
-#     0)
-#         cd ../
-#         ./main_menu.sh
-#         ;;
-#     *) echo "Invalid choice!" ;;
-#     esac
-#     read -n1 -p "Press any key to continue..."
-# done
